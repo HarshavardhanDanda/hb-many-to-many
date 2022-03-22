e682917b864605c7d76ff8b7141412a371d5baaa
Harshazemoso <harshavardhan.danda@zemosolabs.com>
2022-03-22 10:59:23 +0530

    Initial commit

--- src/com/Entities/Review.java
+++ src/com/Entities/Review.java
@@ -1,4 +1,4 @@
-package com.mappings;
+package com.Entities;
 
 import javax.persistence.*;
 
--- src/com/Entities/Student.java
+++ src/com/Entities/Student.java
@@ -1,4 +1,4 @@
-package com.mappings;
+package com.Entities;
 
 import javax.persistence.*;
 import java.util.ArrayList;
